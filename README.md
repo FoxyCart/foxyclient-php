41c94573ce5e692b31cc7a20593445d19e65f02c
Luke Stokes <luke.stokes@gmail.com>
2015-04-02 17:51:50 -0500

    Initial commit of FoxyClient.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # foxyclient
 FoxyClient - a PHP Guzzle based API Client for working with the Foxy Hypermedia API
+
+This project is under development and not ready for public use.
---
+++ src/Foxy/FoxyClient/FoxyClient.php
@@ -0,0 +1,300 @@
+<?php
+
+namespace Foxy\FoxyClient;
+
+/*
+
+The FoxyClient wraps Guzzle with some useful helpers for working with link relationships.
+config:
+    use_sandbox (boolean): set to true to connect to the API sandbox for testing.
+    access_token (string): pass in your properly scoped OAuth access_token to be used as a Authentication Bearer HTTP header.
+
+Examples:
+
+//Get Homepage
+$fc = new FoxyClient($guzzle);
+$result = $fc->get();
+die("<pre>" . print_r($result, 1) . "</pre>");
+
+//Get Authenticated Store
+$fc = new FoxyClient($guzzle, array($access_token));
+$fc->get();
+$result = $fc->get($fc->getLink("fx:store"));
+die("<pre>" . print_r($result, 1) . "</pre>");
+
+Credits: David Hollander of https://foxytools.com/ got this project going and is awesome.
+*/
+class FoxyClient
+{
+    const PRODUCTION_API_HOME = 'https://api.foxycart.com';
+    const SANDBOX_API_HOME = 'https://api-sandbox.foxycart.com';
+
+    /**
+    * OAuth Access Token (can have various scopes such as client_full_access, user_full_access, store_full_access)
+    */
+    private $access_token = '';
+    /**
+    * The timestamp for when the access token expires
+    */
+    private $access_token_expires = '';
+    /**
+    * OAuth Refresh token used to obtain a new Access Token
+    */
+    private $refresh_token = '';
+    /**
+    * OAuth endpoint for getting a new Access Token
+    */
+    private $oauth_token_endpoint = '';
+    /**
+    * Used by OAuth for getting a new Access Token
+    */
+    private $client_id = '';
+    /**
+    * Used by OAuth for getting a new Access Token
+    */
+    private $client_secret = '';
+    private $guzzle;
+    private $method = '';
+    private $last_status_code = '';
+    private $registered_link_relations = array('self', 'first', 'prev', 'next', 'last');
+    private $links = array();
+    private $use_sandbox = false;
+    private $obtaining_updated_access_token = false;
+
+    public function __construct(\GuzzleHttp\Client $guzzle, array $config = null)
+    {
+        $this->guzzle = $guzzle;
+        if (array_key_exists('use_sandbox', $config)) {
+            $this->use_sandbox = $config['use_sandbox'];
+        }
+        if (array_key_exists('access_token', $config)) {
+            $this->access_token = $config['access_token'];
+        }
+        if (array_key_exists('access_token_expires', $config)) {
+            $this->access_token_expires = $config['access_token_expires'];
+        }
+        if (array_key_exists('refresh_token', $config)) {
+            $this->refresh_token = $config['refresh_token'];
+        }
+        if (array_key_exists('client_id', $config)) {
+            $this->client_id = $config['client_id'];
+        }
+        if (array_key_exists('client_secret', $config)) {
+            $this->client_secret = $config['client_secret'];
+        }
+    }
+
+    public function setAccessToken($access_token)
+    {
+        $this->access_token = $access_token;
+    }
+    public function getAccessToken()
+    {
+        return $this->access_token;
+    }
+
+    public function setAccessTokenExpires($access_token_expires)
+    {
+        $this->access_token_expires = $access_token_expires;
+    }
+    public function getAccessTokenExpires()
+    {
+        return $this->access_token_expires;
+    }
+
+    public function setRefreshToken($refresh_token)
+    {
+        $this->refresh_token = $refresh_token;
+    }
+    public function getRefreshToken()
+    {
+        return $this->refresh_token;
+    }
+
+    public function setClientId($client_id)
+    {
+        $this->client_id = $client_id;
+    }
+    public function getClientId()
+    {
+        return $this->client_id;
+    }
+
+    public function setClientSecret($client_secret)
+    {
+        $this->client_secret = $client_secret;
+    }
+    public function getClientSecret()
+    {
+        return $this->client_secret;
+    }
+
+    public function getApiHome()
+    {
+        return $this->use_sandbox ? static::SANDBOX_API_HOME : static::PRODUCTION_API_HOME;
+    }
+
+    public function get($uri = "", $post = null)
+    {
+        $this->method = "GET";
+        return $this->go($uri, $post);
+    }
+
+    public function post($uri, $post = null)
+    {
+        $this->method = "POST";
+        return $this->go($uri, $post);
+    }
+
+    public function patch($uri, $post = null)
+    {
+        $this->method = "PATCH";
+        return $this->go($uri, $post);
+    }
+
+    public function delete($uri, $post = null)
+    {
+        $this->method = "DELETE";
+        return $this->go($uri, $post);
+    }
+
+    public function options($uri, $post = null)
+    {
+        $this->method = "OPTIONS";
+        return $this->go($uri, $post);
+    }
+
+    private function go($uri, $post)
+    {
+        if ($this->access_token && !$this->obtaining_updated_access_token) {
+            $this->refreshTokenAsNeeded();
+        }
+
+        if (!is_array($post)) {
+            $post = null;
+        }
+
+        //Nothing passed in, set uri to homepage
+        if (!$uri) {
+            $uri = $this->getApiHome();
+        }
+
+        //Setup Guzzle Details
+        $guzzle_args = array(
+            'headers' => $this->getHeaders(),
+            'connect_timeout' => 30,
+        );
+
+        //Set Query or Body
+        if ($this->method === "GET" && $post !== null) {
+            $guzzle_args['query'] = $post;
+        } elseif ($post !== null) {
+            $guzzle_args['body'] = $post;
+        }
+
+        $api_request = $this->guzzle->createRequest($this->method, $uri, $guzzle_args);
+        $api_response = $this->guzzle->send($api_request);
+        $this->last_status_code = $api_response->getStatusCode();
+        $data = $api_response->json();
+        $this->saveLinks($data);
+        return $data;
+    }
+
+
+    //Save Links to the Object For Easy Retrieval Later
+    public function saveLinks($data)
+    {
+        if (!isset($data['_links'])) {
+            return;
+        }
+        foreach ($data['_links'] as $key => $val) {
+            $this->links[$key] = $val;
+        }
+    }
+
+
+    //Get a link out of the internally stored links
+    public function getLink($str)
+    {
+        $search_string = $str;
+        if (!in_array($str, $this->registered_link_relations) && strpos($str, "fx:") === FALSE) {
+            $search_string = 'fx:' . $search_string;
+        }
+        if (isset($this->links[$search_string])) {
+            return $this->links[$search_string]['href'];
+        } else {
+            return "";
+        }
+    }
+
+
+    //See if there were any errors
+    public function checkForErrors($data)
+    {
+        if ($this->last_status_code >= 400) {
+            if (isset($data['error_description'])) {
+                return array($data['error_description']);
+            } elseif (isset($data['_embedded']['fx:errors'])) {
+                $errors = array();
+                foreach ($data['_embedded']['fx:errors'] as $error) {
+                    $errors[] = $error['message'];
+                }
+                return $errors;
+            } else {
+                return array("No data returned.");
+            }
+        }
+        return false;
+    }
+
+
+    //Get headers for this call
+    public function getHeaders()
+    {
+        $headers = array(
+            'FOXY-API-VERSION' => 1
+        );
+        if ($this->access_token) {
+            $headers['Authorization'] = "Bearer " . $this->access_token;
+        }
+        return $headers;
+    }
+
+    //Get Last Status Code
+    public function getLastStatusCode()
+    {
+        return $this->last_status_code;
+    }
+
+    public function refreshTokenAsNeeded()
+    {
+        if ($this->client_id || $this->client_secret) {
+            if (!isset($this->access_token_expires) || ($this->access_token_expires - 30) < time()) {
+                $this->obtaining_updated_access_token = true;
+                $refresh_token_data = array(
+                        'grant_type' => 'refresh_token',
+                        'refresh_token' => $this->refresh_token,
+                        'client_id' => $this->client_id,
+                        'client_secret' => $this->client_secret
+                );
+                $data = $this->post($this->getOAuthTokenEndpoint(),$refresh_token_data);
+                if ($this->getLastStatusCode() == '200') {
+                    $this->access_token_expires = time() + $data['expires_in']; 
+                    $this->access_token = $data['access_token'];
+                }
+                $this->obtaining_updated_access_token = false;
+            }
+        }
+    }
+
+    public function getOAuthTokenEndpoint()
+    {
+        if (!isset($oauth_token_endpoint)) {
+            $resp = $this->get();
+            $oauth_token_endpoint = $this->getLink("fx:token");
+        }
+        return $oauth_token_endpoint;
+    }
+
+
+}
